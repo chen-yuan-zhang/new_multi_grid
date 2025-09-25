5f6b123eb210cc7e6e1eafdf0199a122ce86b43d
Sukai Huang <hsk6808065@163.com>
2025-09-25 03:30:40 +0000

    update gitignore

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CUDA_HOME=$CONDA_PREFIX
-export PYTHONPATH=/home/sukai/Project/chenyuan_project/new_multi_grid
-export WORKING_DIR=/home/sukai/Project/chenyuan_project/new_multi_grid
+export PYTHONPATH=/home/sukai/Project/chenyuan_project/new_multi_grid_rl
+export WORKING_DIR=/home/sukai/Project/chenyuan_project/new_multi_grid_rl
 export ACTOR_PREDICTOR_MODEL_PATH=/home/sukai/Project/chenyuan_project/outputs/unsloth_finetune
 export WANDB_DIR=/home/sukai/wandb 
 export WANDB_PROJECT=chenyuan_action_prediction
